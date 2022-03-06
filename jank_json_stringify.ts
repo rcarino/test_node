b699410eca49fb376dc71ddc6d4ef6c20ec3148f
ray <rc@berkeley.edu>
2022-03-05 19:55:25 -0800

    add array support

--- jank_json_stringify.ts
+++ jank_json_stringify.ts
@@ -4,18 +4,21 @@ function jankSONStringify(obj: object): string {
   } else if (obj === undefined) {
     return 'undefined';
   } else if (typeof obj === 'number') {
+    // number and string are the only primitives for now
     return `${obj}`;
   } else if (typeof obj === 'string') {
     return `'${obj}'`;
-    // TODO: arrays
+  } else if (Array.isArray(obj)) {
+    return `[${obj.map(val => jankSONStringify(val)).join(', ')}]`;
   } else {
     // Assuming all other base cases are handled, this is always an obj body.
     const keyVals = Object.entries(obj)
-      .map(([key, value]) => `'${key}': ${jankSONStringify(value)}`).join(',');
+      .map(([key, value]) => `'${key}': ${jankSONStringify(value)}`).join(', ');
     return `{${keyVals}}`
   }
 }
 
-const input = {'a': {'deepObjVal': 2}, 'b': 3};
+const input = {'a': {'deepObjVal': 2}, 'b': 3, 'c': ['d', {'e': 'f'}]};
 console.assert(jankSONStringify(input)
-  === `{'a': {'deepObjVal': 2}, 'b': 3}`, jankSONStringify(input));
+  === `{'a': {'deepObjVal': 2}, 'b': 3, 'c': ['d', {'e': 'f'}]}`,
+  jankSONStringify(input));
